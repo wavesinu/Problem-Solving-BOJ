f27163040cb2ff96c7255c3ca5b3dc5f69e73871
asahilove <yerin851@icloud.com>
2021-10-14 15:02:20 +0900

    20211014
    Practice always.

---
+++ Algotithm/2752.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    vector<int> num;
+    int x;
+
+    for (int i = 0; i < 3; i++){
+        cin >> x;
+        num.push_back(x);
+    }
+    sort(num.begin(), num.end());
+    for (int i = 0; i < num.size(); i++){
+        cout << num[i] << " ";
+    }
+}
