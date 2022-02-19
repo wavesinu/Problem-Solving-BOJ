cded2adf2e26f62e39a3b5cf7caeac4a3cc2ecfe
Asahisuki <yerin851@icloud.com>
2022-02-19 22:06:05 +0900

    19. 02. 22

---
+++ Python/2217(greedy algorithm).py
@@ -0,0 +1,17 @@
+import sys
+
+if __name__ == "__main__":
+    N = int(input())
+    rope = [int(sys.stdin.readline()) for _ in range(N)]
+    rope.sort(reverse=True)
+
+    result = 0
+
+    for i in range(len(rope)):
+        tmp = rope[i] * (i + 1)
+
+        if tmp > result:
+            result = tmp
+
+    print(result)
+    print(len(rope))
